524b72b1c490e64e0f65a5f44c721e6e3f2a67b5
David Bösiger <david.boesiger@bluewin.ch>
2017-03-30 13:12:35 +0200

    small changes

--- js/application.js
+++ js/application.js
@@ -7,8 +7,11 @@ $(document).ready(function(){
     var height = 500;
     var svg = d3.select('#d3-chart').append('svg')
         .attr('width', width)
-        .attr('height', height).on("ontouchstart" in document ? "touchmove" : "mousemove", particle);
+        .attr('height', height);
 
+});
+
+function query(query,handljeData){
     var data = $.param({
         dataset: 'ist-daten-history',
         rows: 10000,
@@ -25,29 +28,8 @@ $(document).ready(function(){
         dataType: 'jsonp',
         data: data,
         success: function (response) {
-          handleData(response);
+            handleData(response);
         }
     });
-});
-
-function handleData(data) {
-
 }
 
-function particle() {
-  var m = d3.mouse(this);
-  d3.select(this).insert("circle")
-      .attr("cx", m[0])
-      .attr("cy", m[1])
-      .attr("r", 1e-6)
-      .style("stroke", d3.hsl((i = (i + 1) % 360), 1, .5))
-      .style("stroke-opacity", 1)
-    .transition()
-      .duration(2000)
-      .ease(Math.sqrt)
-      .attr("r", 100)
-      .style("stroke-opacity", 1e-6)
-      .remove();
-
-  d3.event.preventDefault();
-}
