01aaacb02361bbaefcdd182c7635eb0d7060c6cc
David Bösiger <david.boesiger@bluewin.ch>
2017-05-24 13:01:22 +0200

    added late trains

--- genreate.php
+++ genreate.php
@@ -14,15 +14,31 @@ $newjson = [];
 $entriesyount = count($json);
 foreach($json as $key => $entry){
     $trainstationnumber = $entry['fields']['nummer'];
+
+    // Count Query
     $query = 'SELECT count(*) FROM "ist-daten".history WHERE bpuic = '.$trainstationnumber;
     $result = pg_query($dbconn,$query);
     $trcount = pg_fetch_row($result)[0];
-    $newjson[$key] = [
-        'type'=>'Feature',
-        'name'=>$entry['fields']['name'],
-        'geometry'=>$entry['geometry'],
-        'count'=>intval($trcount)
-    ];
+
+    if($trcount > 0){
+        $versp = -120; // ab wann ist es eine Verspätung
+        // Count Qurey for late trains
+        $query = 'SELECT count(*) FROM "ist-daten".history WHERE bpuic ='.$trainstationnumber.
+            ' AND diff_abfahrt < '.$versp;
+        $result = pg_query($dbconn,$query);
+        $latecount = pg_fetch_row($result)[0];
+        $latepercentage = round(($latecount / $trcount) * 100,2);
+
+
+        $newjson[$key] = [
+            'type'=>'Feature',
+            'name'=>$entry['fields']['name'],
+            'geometry'=>$entry['geometry'],
+            'count'=>intval($trcount),
+            'late'=>$latepercentage
+        ];
+    }
+
     echo 100 * (($key + 1) / $entriesyount) .' %';
     echo "\r\n";
 }
