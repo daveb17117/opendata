08f4521dc78c3707767ba6ded07c31c1dfb1e870
David Bösiger <david.boesiger@bluewin.ch>
2017-05-25 16:17:27 +0200

    change generate to generate entirely from the database

--- genreate.php
+++ genreate.php
@@ -6,14 +6,14 @@ if(!$dbconn){
     exit;
 }
 
-// Get Trainstation JSON as array
-$tsstring = file_get_contents(__DIR__.'/trainstation.json');
-$json = json_decode($tsstring,true);
-
+// Get Trainstations which are in the database
+$query = 'SELECT DISTINCT ON(bpuic) bpuic, haltestellen_name, geopos FROM "ist-daten".history WHERE geopos IS NOT NULL';
+$result_ts = pg_query($dbconn,$query);
 $newjson = [];
-$entriesyount = count($json);
-foreach($json as $key => $entry){
-    $trainstationnumber = $entry['fields']['nummer'];
+$entriescount = pg_num_rows($result_ts);
+$itrcount = 1;
+while($row = pg_fetch_row($result_ts)){
+    $trainstationnumber = intval($row[0]);
 
     // Count Query
     $query = 'SELECT count(*) FROM "ist-daten".history WHERE bpuic = '.$trainstationnumber;
@@ -35,11 +35,13 @@ foreach($json as $key => $entry){
         $outcount = pg_fetch_row($result)[0];
         $outpercentage = round(($outcount / $trcount) * 100,2);
 
+        $geopos = explode(',',str_replace('}','',str_replace('{','',$row[2])));
+
 
         $newjson[] = [
             'type'=>'Feature',
-            'name'=>$entry['fields']['name'],
-            'geometry'=>$entry['geometry'],
+            'name'=>$row[1],
+            'geometry'=>["type"=>"Point","coordinates"=>[floatval($geopos[1]),floatval($geopos[0])]],
             'count'=>intval($trcount),
             'latecount'=>intval($latecount),
             'late'=>$latepercentage,
@@ -48,13 +50,13 @@ foreach($json as $key => $entry){
         ];
     }
 
-    echo 100 * (($key + 1) / $entriesyount) .' %';
+    echo 100 * ($itrcount / $entriescount) .' %';
     echo "\r\n";
+    $itrcount++;
 }
 
-
 $output = json_encode($newjson,JSON_UNESCAPED_UNICODE);
-file_put_contents( __DIR__.'/tnew.json',$output);
+file_put_contents( __DIR__.'/trial.json',$output);
 
 echo 'success';
 ?>
